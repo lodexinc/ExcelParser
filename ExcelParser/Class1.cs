13d642a3374efd689a7666668d07700ab334b594
Tim Rayburn <tim@timrayburn.net>
2015-08-31 11:06:00 -0500

    Initial Commit

---
+++ ExcelParser/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelParser
+{
+    public interface IExcelParser
+    {
+        T Parse<T>(string fileName) where T : new();
+    }
+
+    public class BaseExcelParser 
+}
